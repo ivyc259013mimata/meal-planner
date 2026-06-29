406ed958fdc1d300bccf2bba50180f9648530536
mimata3 <c259013@ivy.ac.jp>
2026-06-29 15:33:42 +0900

    RecipeControllerのCRUD機能(edit・update・destroy)完成

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class RecipeController extends Controller
 {
-    public function index(Request $request)
+    public function index(Request $request)//一覧・検索
     {
         if($request->search != null) {
             $recipes = Recipe::where('name', 'like', '%' . $request->search . '%')->get();
@@ -14,5 +14,39 @@ class RecipeController extends Controller
             $recipes = Recipe::all();
         }
         return view('recipe.index', compact('recipes'));
+    }
+
+    public function store(Request $request)//新規登録
+    {
+        Recipe::create([
+            'name' => $request->name
+        ]);
+
+        session()->flash('message', 'レシピを保存しました');
+
+        return redirect()->back();//連続登録
+    }
+
+    public function edit($id)//編集画面表示
+    {
+        $recipe = Recipe::find($id);
+        return view('recipe.edit', compact('recipe'));
+    }
+
+    public function update(Request $request, $id)//更新
+    {
+        $recipe = Recipe::find($id);
+        $recipe->name = $request->name;
+        $recipe->category = $request->category;
+        $recipe->save();
+
+        return redirect('/recipe');
+    }
+
+    public function destroy($id)//削除
+    {
+        $recipe = Recipe::find($id);
+        $recipe->delete();
+        return redirect('/recipe');
     }
 }
