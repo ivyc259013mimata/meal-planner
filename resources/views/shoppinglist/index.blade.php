f3b5cb732d350c84babca7ba959f735139ff8e93
mimata3 <c259013@ivy.ac.jp>
2026-09-01 13:27:06 +0900

    買い物リストにチェックボックス機能を実装、is_checkedをDBに保存できるようにした

--- resources/views/shoppinglist/index.blade.php
+++ resources/views/shoppinglist/index.blade.php
@@ -1,7 +1,20 @@
-<h1>買い物リスト</h1>
+@extends('layouts.app')
 
-<ul>
-    @foreach ($shoppingList as $name => $count)
-        <li>{{ $name }} *{{ $count }}</li>
-    @endforeach
-</ul>
+@section('title','買い物リスト')
+
+@section('content')
+
+    <ul>
+        @foreach ($shoppingList as $name => $item)
+            <li>
+                <form action="/ingredient/{{ $item['id'] }}/toggle" method="POST">
+                    @csrf
+                    <input type="checkbox" onchange="this.form.submit()"
+                        {{ $item['is_checked']? 'checked' : '' }}>
+                    {{ $name }} ×{{ $item['count'] }}
+                </form>
+            </li>
+        @endforeach
+    </ul>
+
+@endsection
