548fd04aa89b35c1c8a6c856c579e6d8a20f2aec
mimata3 <c259013@ivy.ac.jp>
2026-09-07 15:09:51 +0900

    レシピ登録画面のCSSを実装

--- resources/views/recipe/create.blade.php
+++ resources/views/recipe/create.blade.php
@@ -1,32 +1,43 @@
-<form action="/recipe/store" method="POST">
-    @csrf
-    <div>
-        <label>レシピ名</label>
-        <input type="text" name="name">
-    </div>
-    <div>
-        <label>カテゴリ</label>
-        <input type="text" name="category">
-    </div>
-    <div>
-        <label>種類</label>
-        <select name="dish_type">
-            <option value="主菜">主菜</option>
-            <option value="副菜">副菜</option>
-        </select>
-    </div>
-    <div>
-        <label>材料</label>
-        @foreach ($ingredients as $ingredient)
-            <div>
-                <input type="checkbox"
-                       name="ingredients[]"
-                       value="{{ $ingredient->id }}">
-                {{-- name="ingredients[]" の[]は「複数選べる」という意味 --}}
-                {{-- value にはidを入れる（どの材料か識別するため） --}}
-                <label>{{ $ingredient->name }}</label>
+@extends('layouts.app')
+
+@section('title', 'レシピを登録')
+
+@section('content')
+
+<div class="form-card">
+    <form action="/recipe/store" method="POST">
+        @csrf
+        <div>
+            <label>レシピ名</label>
+            <input type="text" name="name">
+        </div>
+        <div>
+            <label>ジャンル</label>
+            <select name="category">
+                <option value="和食">和食</option>
+                <option value="洋食">洋食</option>
+            </select>
+        </div>
+        <div>
+            <label>種類</label>
+            <select name="dish_type">
+                <option value="主菜">主菜</option>
+                <option value="副菜">副菜</option>
+            </select>
+        </div>
+        <div>
+            <label>材料</label>
+            <div class="ingredient-tags">
+                @foreach ($ingredients as $ingredient)
+                    <label class="ingredient-tag">
+                        <input type="checkbox" name="ingredients[]" value="{{ $ingredient->id }}" class="ingredient-tag__checkbox">
+                        <span class="ingredient-tag__label">{{ $ingredient->name }}</span>
+                    </label>
+                @endforeach
             </div>
-        @endforeach
-    </div>
-    <button type="submit">保存</button>
-</form>
+        </div>
+        <button type="submit" class="btn-save">保存</button>
+    </form>
+</div>
+
+@endsection
